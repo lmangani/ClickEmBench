70008bc5f70e35562a8f0298607d4622cbb317a9
root <root@localhost.localdomain>
2023-11-15 17:09:10 +0100

    resync

--- duckdb/convert.sh
+++ duckdb/convert.sh
@@ -3,7 +3,7 @@
 PROVIDER="${PROVIDER:-github_runner}"
 MACHINE="${MACHINE:-2vCPU_7GB}"
 
-RESULTS=$(cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' |     awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }') # >> results.json
+RESULTS=$(cat log.txt | awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }') # >> results.json
 
 mkdir -p results
 
--- duckdb/query.py
+++ duckdb/query.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-#!/usr/bin/env python3
-
 import duckdb
 import timeit
 import psutil
