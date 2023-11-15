31f591a036bc0e0a879aea7468ed986ef0eece0d
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-15 15:10:40 +0100

    Update convert.sh

--- duckdb/convert.sh
+++ duckdb/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROVIDER="${PROVIDER:-github}"
-MACHINE="${MACHINE:-2cpu7gb}"
+PROVIDER="${PROVIDER:-github_runner}"
+MACHINE="${MACHINE:-2vCPU 7GB}"
 
 RESULTS=$(cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' |     awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }') # >> results.json
 
@@ -9,21 +9,17 @@ mkdir -p results
 
 VERSION=$(python3 -c "import duckdb; print(duckdb.__version__)")
 
-echo '
-{
-    "system": "DuckDB ('$VERSION')",
+echo '{
+    "system": "DuckDB",
     "date": "'$(date +%F)'",
-    "machine": "'$PROVIDER-$MACHINE'",
+    "machine": "'$PROVIDER $MACHINE'",
     "cluster_size": "serverless",
-    "comment": "",
-
+    "comment": "'$VERSION'",
     "tags": ["C++", "column-oriented", "embedded", "github"],
-
     "load_time": '0',
     "data_size": '$(find . -type f -name "*.parquet" -print0 | xargs -0r du -cb | tail -n1 | awk '{print $1}')',
-
     "result": [
-'$(echo $RESULTS | head -c-2)'
-]
+      '$(echo $RESULTS | head -c-2)'
+    ]
 }
 ' > "results/${PROVIDER}.${MACHINE}.json"
