1ff0940bf622818ba11b0b138149f461e40202c5
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-15 17:44:06 +0100

    Update convert.sh

--- duckdb/convert.sh
+++ duckdb/convert.sh
@@ -3,7 +3,9 @@
 PROVIDER="${PROVIDER:-github_runner}"
 MACHINE="${MACHINE:-2vCPU_7GB}"
 
-RESULTS=$(cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' | awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }')
+cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' | awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }' > dump.json 
+RESULTS=$(cat dump.json)
+rm -rf dump.json
 
 mkdir -p results
 
