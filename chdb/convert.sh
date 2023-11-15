faf8554888a5efdb3ac20fa76a2dc5e565097529
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-15 15:09:08 +0100

    Update convert.sh

--- chdb/convert.sh
+++ chdb/convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PROVIDER="${PROVIDER:-github}"
-MACHINE="${MACHINE:-2cpu7gb}"
+PROVIDER="${PROVIDER:-github_runner}"
+MACHINE="${MACHINE:-2vCPU 7GB}"
 
 RESULTS=$(cat log.txt | grep -P '^\d|Killed|Segmentation' | sed -r -e 's/^.*(Killed|Segmentation).*$/null\nnull\nnull/' |     awk '{ if (i % 3 == 0) { printf "[" }; printf $1; if (i % 3 != 2) { printf "," } else { print "]," }; ++i; }') # >> results.json
 
@@ -13,13 +13,11 @@ echo '
 {
     "system": "chdb ('$(echo $VERSION)')",
     "date": "'$(date +%F)'",
-    "machine": "'$PROVIDER-$MACHINE'",
-    "cluster_size": "serverless",
+    "machine": "'$PROVIDER $MACHINE'",
+    "cluster_size": "1",
     "comment": "",
-
     "tags": ["C++", "column-oriented", "ClickHouse derivative", "embedded", "github"],
-
-    "load_time": '0',
+    "load_time": "0",
     "data_size": '$(find . -type f -name "*.parquet" -print0 | xargs -0r du -cb | tail -n1 | awk '{print $1}')',
 
     "result": [
