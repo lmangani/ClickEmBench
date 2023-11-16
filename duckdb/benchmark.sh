3d14e840ac6ca2c7d689593752440d65b7589085
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2023-11-16 14:54:40 +0100

    Update benchmark.sh
    Signed-off-by: Lorenzo Mangani <lorenzo.mangani@gmail.com>

--- duckdb/benchmark.sh
+++ duckdb/benchmark.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Install
-
 sudo apt-get update
 sudo apt-get install -y python3-pip
 pip install duckdb psutil
 
+# Dataset Size
+PARQUETS="${PARQUETS:-1}"
+
 # Load the data
-seq 0 1 | xargs -P100 -I{} bash -c 'wget --no-verbose --continue https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
+seq 0 $PARQUETS | xargs -P100 -I{} bash -c 'wget --no-verbose --continue https://datasets.clickhouse.com/hits_compatible/athena_partitioned/hits_{}.parquet'
 
 # Run the queries
 
